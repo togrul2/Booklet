47c0b4a3b09be88b0cecc2f53ef16dbb51e8edb7
togrul2 <adsads00123@gmail.com>
2025-01-30 18:28:54 +0400

    Fixed meaningless validation error descriptions.

--- src/main/java/com/github/togrul2/booklet/exceptions/GlobalExceptionAdvice.java
+++ src/main/java/com/github/togrul2/booklet/exceptions/GlobalExceptionAdvice.java
@@ -4,15 +4,24 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * This class is used to handle exceptions that are thrown in the application.
@@ -28,16 +37,30 @@ import java.io.IOException;
  * @see EntityNotFoundException
  * @since 1.0
  */
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionAdvice.class);
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request
+    ) {
+        log.warn("Validation exception: {}", ex.getMessage());
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        Map<String, Set<String>> errorsMap = fieldErrors.stream().collect(
+                Collectors.groupingBy(FieldError::getField,
+                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.toSet())
+                )
+        );
+        return ResponseEntity.badRequest().body(errorsMap);
+    }
 
     /**
      * Handle AccessDeniedException. An exception is thrown when a user tries to access a resource that is not allowed.
      */
     @ExceptionHandler(AccessDeniedException.class)
     private void handleAccessDeniedException(RuntimeException e, HttpServletResponse response) throws IOException {
-        logger.warn("Access denied exception: {}", e.getMessage());
+        log.warn("Access denied exception: {}", e.getMessage());
         response.sendError(HttpStatus.FORBIDDEN.value());
     }
 
@@ -47,7 +70,7 @@ public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
      */
     @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
     private void handleConflictException(RuntimeException e, HttpServletResponse response) throws IOException {
-        logger.warn("Conflict exception: {}", e.getMessage());
+        log.warn("Conflict exception: {}", e.getMessage());
         response.sendError(HttpStatus.CONFLICT.value());
     }
 
@@ -57,7 +80,7 @@ public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
      */
     @ExceptionHandler(JwtException.class)
     private void handleJwtException(RuntimeException e, HttpServletResponse response) throws IOException {
-        logger.warn("JWT exception: {}", e.getMessage());
+        log.warn("JWT exception: {}", e.getMessage());
         response.sendError(HttpStatus.UNAUTHORIZED.value());
     }
 
@@ -67,7 +90,7 @@ public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
      */
     @ExceptionHandler(EntityNotFoundException.class)
     private void handleEntityNotFoundException(RuntimeException e, HttpServletResponse response) throws IOException {
-        logger.warn("Entity not found exception: {}", e.getMessage());
+        log.warn("Entity not found exception: {}", e.getMessage());
         response.sendError(HttpStatus.NOT_FOUND.value());
     }
 }
